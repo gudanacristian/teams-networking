d0440d995570e8605cb2eff344d15c4b94e27563
Guana Cristian Bogdan <gudanacristian@gmail.com>
2022-08-06 16:45:20 +0300

    extract getTeamHTML function

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+function getTeamHTML(team) {
+  return `
+  <tr>
+      <td>${team.promotion}</td>
+      <td>${team.members}</td>
+      <td>${team.name}</td>
+      <td>
+        <a href="${team.url}">open</a>
+      </td>
+      <td>x e</td>
+  </tr>`;
+}
+
 function displayTeams(teams) {
   //transforma in html
   var teamsHTML = "";
   teams.forEach(function (team) {
     console.info(team.promotion);
-    teamsHTML += `
-      <tr>
-          <td>${team.promotion}</td>
-          <td>${team.members}</td>
-          <td>${team.name}</td>
-          <td>
-            <a href="${team.url}">open</a>
-          </td>
-          <td>x e</td>
-      </tr>`;
+    teamsHTML += getTeamHTML(team);
   });
 
   // afisare
