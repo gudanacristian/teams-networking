cfb987b481ccf18aee549c625bd1bdb70aee36b4
Guana Cristian Bogdan <gudanacristian@gmail.com>
2022-08-06 15:49:35 +0300

    first teams list version

---
+++ index.js
@@ -0,0 +1,33 @@
+function displayTeams(teams) {
+  //transforma in html
+  var teamsHTML = "";
+  teams.forEach(function (team) {
+    console.info(team.promotion);
+    teamsHTML += `
+      <tr>
+          <td>${team.promotion}</td>
+          <td>${team.members}</td>
+          <td>${team.name}</td>
+          <td>
+            <a href="${team.url}">open</a>
+          </td>
+          <td>x e</td>
+      </tr>`;
+  });
+
+  // afisare
+  document.querySelector("table tbody").innerHTML = teamsHTML;
+}
+
+function loadTeams() {
+  fetch("data/teams.json")
+    .then(function (r) {
+      console.info(r);
+      return r.json();
+    })
+    .then(function (teams) {
+      displayTeams(teams);
+    });
+}
+
+loadTeams();
