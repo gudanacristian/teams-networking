951cf55705af06b9698b2e8a6e59f3c0caa72d8e
Guana Cristian Bogdan <gudanacristian@gmail.com>
2022-10-13 19:33:11 +0300

    return to json

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function displayTeams(teams) {
 }
 
 function loadTeams() {
-  fetch("http://localhost:3000/teams")
+  fetch("http://localhost:3000/teams-json")
     .then((r) => r.json())
     .then((teams) => {
       allTeams = teams;
@@ -39,7 +39,7 @@ function loadTeams() {
 }
 
 function createTeamRequest(team) {
-  return fetch("http://localhost:3000/teams/create", {
+  return fetch("http://localhost:3000/teams-json/create", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -49,7 +49,7 @@ function createTeamRequest(team) {
 }
 
 function removeTeamRequest(id) {
-  return fetch("http://localhost:3000/teams/delete", {
+  return fetch("http://localhost:3000/teams-json/delete", {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
@@ -59,7 +59,7 @@ function removeTeamRequest(id) {
 }
 
 function updateTeamRequest(team) {
-  return fetch("http://localhost:3000/teams/update", {
+  return fetch("http://localhost:3000/teams-json/update", {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
